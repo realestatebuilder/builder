5e3c2be3d3311a21d233f67671457f2790db54af
Atharva Deshpande <34884015+AD1310@users.noreply.github.com>
2020-07-09 19:27:45 +0530

    admin panel cat functions removed

--- admin_area/index.php
+++ admin_area/index.php
@@ -123,54 +123,6 @@ include("edit_product.php");
 
 }
 
-if(isset($_GET['insert_p_cat'])){
-
-include("insert_p_cat.php");
-
-}
-
-if(isset($_GET['view_p_cats'])){
-
-include("view_p_cats.php");
-
-}
-
-if(isset($_GET['delete_p_cat'])){
-
-include("delete_p_cat.php");
-
-}
-
-if(isset($_GET['edit_p_cat'])){
-
-include("edit_p_cat.php");
-
-}
-
-if(isset($_GET['insert_cat'])){
-
-include("insert_cat.php");
-
-}
-
-if(isset($_GET['view_cats'])){
-
-include("view_cats.php");
-
-}
-
-if(isset($_GET['delete_cat'])){
-
-include("delete_cat.php");
-
-}
-
-if(isset($_GET['edit_cat'])){
-
-include("edit_cat.php");
-
-}
-
 
 
 if(isset($_GET['view_customers'])){
